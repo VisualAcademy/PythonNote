bc69fe25da40c1819982ca6c184f91969f89c08a
AzureCamp <azcamp@outlook.com>
2019-05-10 04:10:26 +0900

    새로운 파일 추가

---
+++ PythonNote/ListDemo.py
@@ -0,0 +1,17 @@
+# 리스트(배열, 컬렉션)
+colors = ["red", "green", "blue"]
+
+print(colors[0])
+print(colors[1])
+print(colors[2])
+
+print(colors[-1]) # blue
+print(colors[-2]) # green
+print(colors[-3]) # red
+
+print(len(colors)) # 3
+
+print(colors[1:]) # 1 인덱스 이후로 가져오기
+
+for color in colors:
+    print(color)
