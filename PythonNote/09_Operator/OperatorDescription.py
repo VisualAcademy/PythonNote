96d87593368e1e8ab48b46d76e7561b06b8883b9
RedPlus <redplus@redplus.net>
2019-07-27 22:44:16 +0900

    파일 및 폴더 정리하는 중...

---
+++ PythonNote/09_Operator/OperatorDescription.py
@@ -0,0 +1,7 @@
+print(3 + 5)
+print(3 - 5)
+print(3 * 5)
+print(3 / 5)
+print(3 % 5) # 나머지
+print(6 // 5) # 몫
+print(2 ** 10) # 2 ^ 10 = 1024
---
+++ PythonNote/27_String/StringFunction.py
@@ -0,0 +1,8 @@
+message = "hello world"
+
+print(message)
+
+print(message.upper()) # 대문자
+print(message.lower()) # 소문자
+
+print(message.replace("hello", "안녕하세요"))
