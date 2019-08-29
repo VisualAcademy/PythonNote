46a883626c5bcea908f2dd5f75efa19c02b45484
RedPlus <redplus@redplus.net>
2019-08-30 04:51:41 +0900

    선택 정렬 알고리즘 강의 소스 추가

--- PythonNote/32_Algorithms/08_SortAlgorithm/SortAlgorithm.py
+++ PythonNote/32_Algorithms/08_SortAlgorithm/SortAlgorithm.py
@@ -1,16 +1,22 @@
-#[1] Input
-numbers = [3, 2, 1, 5, 4]
-N = len(numbers)
+#[?] 무작위 데이터를 순서대로 [오름차순(ASC)|내림차순(DESC)] 정렬 
 
-#[2] Process: Selection Sort(선택 정렬) 알고리즘
-for i in range(N - 1):
-    for j in range(i + 1, N):
-        if (numbers[i] > numbers[j]):
-            temp = numbers[i]
-            numbers[j] = numbers[j + 1]
-            numbers[j] = temp
+# 정렬 알고리즘(Sort Algorithm): 가장 [작은|큰] 데이터를 왼쪽으로 순서대로 이동
+def main():
+    #[1] Input: Data Structure(Array, List, Stack, Queue, Tree, DB, ...)
+    data = [ 3, 2, 1, 5, 4 ] # 정렬되지 않은 데이터 
+    N = len(data) # 의사코드(슈도코드) 형태로 알고리즘을 표현하기 위함
+    # print(data)
 
-#[2] Output
-for i in range(len(numbers)):
-    print(numbers[i])
+    #[2] Process: Selection Sort(선택 정렬) 알고리즘
+    for i in range(0, N - 1): # i = 0 to N - 1
+        for j in range(i + 1, N): # j = i + 1 to N 
+            if data[i] > data[j]: # 부등호 방향: 오름차순(>), 내림차순(<)
+                temp = data[i]; data[i] = data[j]; data[j] = temp # SWAP
+        # print(data)
 
+    #[3] Output: UI(Console, Desktop, Web, Mobile, ...)
+    for i in range(N):
+        print(data[i]) # print(data)
+
+if __name__ == "__main__":
+    main()
