e801864dfa3ecf825b29ab7a83432422b740e6e9
RedPlus <redplus@redplus.net>
2019-09-17 08:10:52 +0900

    코드 정리

--- PythonNote/24_DateTime/DateTimeDemo.py
+++ PythonNote/24_DateTime/DateTimeDemo.py
@@ -1,6 +1,7 @@
 from datetime import date
 from datetime import time
-from datetime import datetime
+from datetime import datetime, timedelta
+
 
 today = date.today()
 
@@ -12,3 +13,17 @@ print("요일 인덱스: ", today.weekday()) # 0(월) ~ 6(일)
 
 print("날짜 전체: ", datetime.now())
 print("시간 전체: ", datetime.time(datetime.now()))
+
+
+print("오늘: " + str(datetime.now()))
+
+one_day = timedelta(days=1) # timedelta == TimeSpan
+
+yesterday = today - one_day
+
+print("어제: " + str(yesterday))
+
+
+birthday = input("생일: (dd/mm/yyyy)? ")
+birthday_date = datetime.strptime(birthday, "%d/%m/%Y")
+print("생일: " + str(birthday_date)) # yyyy-MM-dd
