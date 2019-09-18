4d7bfe73d0c356c653f3780d4526963caf3b963e
RedPlus <redplus@redplus.net>
2019-09-18 11:04:51 +0900

    등차수열

--- PythonNote/32_Algorithms/ArithmeticSequence/ArithmeticSequence.py
+++ PythonNote/32_Algorithms/ArithmeticSequence/ArithmeticSequence.py
@@ -9,7 +9,7 @@ sum = 0
 for i in range(1, 20): # 주어진 범위
     if i % 2 != 0: # 주어진 조건: 필터링(홀수)
         sum = sum + i
-        print(i, end=' ')
+        print(i, end=' ') # SEQUENCE -> Arithmetic Sequence
 
 #[3] Output
 print(f'\n1부터 20까지의 홀수의 합: {sum}')
