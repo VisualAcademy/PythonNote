e5fbcd49e6a9eec93ac3d60fa040e5abb0eda791
Park YongJune <redplus@redplus.net>
2021-11-29 14:11:16 +0900

    .

---
+++ PythonNote/62_Modern/MatchCase.py
@@ -0,0 +1,15 @@
+﻿# 파이썬 패턴 매칭: match case 문: Python 3.10 이상에서 테스트
+shape = "정사각형"
+result = ""
+
+match shape:
+    case "정사각형":
+        result = "Square"
+    case "원":
+        result = "Circle"
+    case "오각형" | "팔각형"
+        result = "Polygon"
+    case _:
+        result = "Other"
+
+print(result)
