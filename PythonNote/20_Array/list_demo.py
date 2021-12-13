94e57c2579a9d58048ac90bb31b94c73aa861199
Park YongJune <redplus@redplus.net>
2021-12-14 08:40:55 +0900

    40_06_Python 강의_매개변수도 있고 반환값도 있는 경우의 람다 식

---
+++ PythonNote/20_Array/list_demo.py
@@ -0,0 +1,9 @@
+# 리스트 생성
+numbers = [1, 3, 5]
+print(numbers)
+
+# 리스트의 값을 for 문 사용 출력
+colors = ["red", "green", "blue"]
+for color in colors:
+    print(color)
+
---
+++ PythonNote/40_Lambda/LambdaExpressionArgs.py
@@ -0,0 +1,7 @@
+﻿#[?] 람다식: 매개 변수도 있고 반환값도 있는 경우
+
+#[1] 매개 변수도 있고 반환값도 있는 람다 식 선언
+square = lambda x: x * x
+
+print(square(3)) # 9
+print(square(4)) # 16
