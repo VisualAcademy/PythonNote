883081bd21914cc266314aed1e9922e1a757d700
Park YongJune <redplus@redplus.net>
2023-03-05 08:32:12 +0900

    관계연산자.py

---
+++ PythonNote/11_RelationalLogical/관계연산자.py
@@ -0,0 +1,9 @@
+first_num = 3
+second_num = 5
+greater = first_num > second_num
+smaller = first_num < second_num
+print(f"first_num = {first_num} second_num = {second_num} "
+      f"first_num > second_num = {greater} "
+      f"first_num < second_num = {smaller}")
+
+# first_num = 3 second_num = 5 first_num > second_num = False first_num < second_num = True
