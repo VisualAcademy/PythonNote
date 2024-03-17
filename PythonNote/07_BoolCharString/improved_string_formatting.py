7246982efd3e922060c3cd6f7e6aab6222002524
Park YongJune <redplus@redplus.net>
2024-03-18 00:36:07 +0900

    Python 문자열 포매팅 개선하기

---
+++ PythonNote/07_BoolCharString/improved_string_formatting.py
@@ -0,0 +1,9 @@
+﻿# 파일명: improved_string_formatting.py
+
+# dog_name = "도그"
+# print(f"My dog's name is {dog_name}. " + dog_name + " is cute.")
+
+# 파일명: improved_string_formatting.py
+
+dog_name = "도그"
+print(f"My dog's name is {dog_name}. {dog_name} is cute.")
